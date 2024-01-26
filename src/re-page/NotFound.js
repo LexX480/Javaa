87bc5a1c04669463eef09e169e90859a7ccfc63e
LexX480 <voidbaka47@gmail.com>
2024-01-26 09:18:54 +0545

    chs

--- src/re-page/NotFound.js
+++ src/re-page/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div className='flex justify-center items-center space-y-2'>
       <div className='h-[200px]'>
-        <dotlottie-player src="https://lottie.host/8f9f8f28-1109-4205-a467-da967e632fed/6htL9zZNIj.json" background="transparent" speed="1" loop autoplay></dotlottie-player>
+        <dotlottie-player src="https://lottie.host/c128d98c-1e95-4015-a621-24f89c93004e/ZWf0uki1d9.json" background="transparent" speed="1" loop autoplay></dotlottie-player>
 
       </div>
 
