cc183b3303bf051333b59b078f2212cda5a60f29
LexX480 <voidbaka47@gmail.com>
2024-01-28 08:26:36 +0545

    rts

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import HomePg from './re-page/HomePg'
 import About from './re-page/About'
 import Contact from './re-page/Contact'
 import NotFound from './re-page/NotFound'
+import Header from './re-page/comps/Header'
 
 const App = () => {
   return (
     <div>
-
+      <Header />
       <Routes>
         <Route path='/' element={<HomePg />} />
         <Route path='About' element={<About />} />
---
+++ src/re-page/comps/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between bg-black text-white px-2 py-2'>
+      <h1>DEMO</h1>
+      <nav className='space-x-3'>
+
+        <NavLink to='/About'>About</NavLink>
+        <NavLink to='/Contact'>Contact</NavLink>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
