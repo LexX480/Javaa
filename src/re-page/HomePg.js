74736bec1ca11d3fc61e8c1bb06d821dff25051b
LexX480 <voidbaka47@gmail.com>
2024-01-29 15:12:40 +0545

    cards

--- src/re-page/HomePg.js
+++ src/re-page/HomePg.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import CardDisplay from './comps/CardDisplay'
+
 
 const HomePg = () => {
   return (
     <div>
+
+<CardDisplay />
       <h1>this is home page</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime autem aliquam eveniet aperiam, tempore veniam sint possimus ab hic deserunt.</p>
+    
+    
     </div>
+    
   )
 }
 
