f42fe73f3ade85435671a5ca8f182adbef0952bb
LexX480 <voidbaka47@gmail.com>
2025-04-20 21:51:20 +0545

    change

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <nav className="bg-blue-600 text-white shadow-lg">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
-        <Link to="/" className="text-2xl font-bold">MyBlog</Link>
+        <Link to="/" className="text-2xl font-bold">Blog</Link>
         <div className="space-x-4">
           <Link to="/" className="hover:text-blue-200">Home</Link>
           <Link to="/create" className="hover:text-blue-200">Create Post</Link>
